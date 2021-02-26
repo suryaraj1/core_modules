52068ea019709912eb8eb53733ea95f8d7f6adf6
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-02-26 16:26:27 +0530

    learning http module

--- http.js
+++ http.js
@@ -1,4 +1,20 @@
 /* eslint-disable no-console */
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello node');
+        res.write(' Hello node, how are you?');
+        res.end();
+    } else if (req.url === '/about') {
+        res.write('I am Suryaraj Bhaduri');
+        res.end();
+    } else {
+        res.write('Not found');
+        res.end();
+    }
+});
+
+server.listen(3000);
+
+console.log('Server is listening on port 3000');
