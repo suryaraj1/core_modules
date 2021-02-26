c28a2ead02bec0392e390aab375a2b9a6e28601a
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-02-26 15:03:23 +0530

    added os module

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-console */
+// core modules :
+// 1. path module
+// 2. OS module
+const path = require('path');
+const os = require('os');
+
+const myPath = '/mnt/d/learn-node/core_modules/index.js';
+
+console.log(os.platform());
+console.log(os.freemem());
+console.log(os.cpus());
